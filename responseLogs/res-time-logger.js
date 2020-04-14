875193eb78f7302765d4b16aca74db46fce24f1d
= <=>
2020-04-14 21:47:09 +0300

    changed log format

--- responseLogs/res-time-logger.js
+++ responseLogs/res-time-logger.js
@@ -7,7 +7,7 @@ function ResponseTimeLogger(req, res, next) {
     const elapsedHrTime = process.hrtime(startHrTime);
     const elapsedTimeInMs = Math.round(elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6);
 
-    fs.appendFile('./responseLogs/response-time-log.txt', `${req.method}\t\t${req.originalUrl}\t\t${res.statusCode}\t\t${elapsedTimeInMs}ms\n`, (err) => {
+    fs.appendFile('./responseLogs/response-time-log.txt', `${req.method}\t\t${req.originalUrl}\t\t${res.statusCode}\t\t0${elapsedTimeInMs}ms\n`, (err) => {
       if (err) throw err;
     });
   });
